d5d511cc0e453d56579b26bd17265e8adb60c362
MysteryS1 S <mysterys1012001@gmail.com>
2023-04-25 02:55:36 +0000

    push-18

--- App/main.py
+++ App/main.py
@@ -44,9 +44,7 @@ def create_app(config_overrides={}):
     app.app_context().push()
 
     with app.app_context():
-        db.drop_all()
         db.create_all()
-        get_data()
     return app
 
     
